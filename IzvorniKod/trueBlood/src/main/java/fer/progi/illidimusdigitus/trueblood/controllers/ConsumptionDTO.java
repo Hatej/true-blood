eabed62c366013694951379113c9feebafbbe198
David Kerman <dk522014@outlook.com>
2021-12-17 00:09:01 +0100

    Impl potrosnja krvi

---
+++ IzvorniKod/trueBlood/src/main/java/fer/progi/illidimusdigitus/trueblood/controllers/ConsumptionDTO.java
@@ -0,0 +1,57 @@
+package fer.progi.illidimusdigitus.trueblood.controllers;
+
+public class ConsumptionDTO {
+	
+	String bloodType;
+	
+	String timestamp;
+	
+	int quantity;
+	
+	String location;
+	
+	String employee;
+
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(String employee) {
+		this.employee = employee;
+	}
+	
+	
+	
+}
