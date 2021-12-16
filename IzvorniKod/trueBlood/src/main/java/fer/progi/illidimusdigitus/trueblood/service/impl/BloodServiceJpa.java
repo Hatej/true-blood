924bbbb82b6c7c7df14e4822873df8f26b960999
David Kerman <dk522014@outlook.com>
2021-12-17 00:08:06 +0100

    Impl smanjenje krvi i podesavanje granica

--- IzvorniKod/trueBlood/src/main/java/fer/progi/illidimusdigitus/trueblood/service/impl/BloodServiceJpa.java
+++ IzvorniKod/trueBlood/src/main/java/fer/progi/illidimusdigitus/trueblood/service/impl/BloodServiceJpa.java
@@ -1,6 +1,9 @@
 package fer.progi.illidimusdigitus.trueblood.service.impl;
 
+import fer.progi.illidimusdigitus.trueblood.controllers.BloodDTO;
+import fer.progi.illidimusdigitus.trueblood.controllers.ConsumptionDTO;
 import fer.progi.illidimusdigitus.trueblood.model.Blood;
+import fer.progi.illidimusdigitus.trueblood.model.User;
 import fer.progi.illidimusdigitus.trueblood.model.util.BloodType;
 import fer.progi.illidimusdigitus.trueblood.repository.BloodRepository;
 import fer.progi.illidimusdigitus.trueblood.service.BloodService;
@@ -25,4 +28,36 @@ public class BloodServiceJpa implements BloodService {
     public List<Blood> findAll() {
        return bloodRepo.findAll();
     }
+    
+    public boolean updateBounds(BloodType type,BloodDTO bloodDTO){
+
+        Optional<Blood> blood = findByName(type);
+
+        if(blood.isEmpty())
+            return false;
+
+        Blood currBlood = blood.get();
+        currBlood.setLowerbound(bloodDTO.getLowerbound());
+        currBlood.setUpperbound(bloodDTO.getUpperbound());
+        bloodRepo.save(currBlood);
+
+        return true;
+    }
+
+	@Override
+	public boolean consume(BloodType type, ConsumptionDTO dto) {
+		Optional<Blood> blood = findByName(type);
+
+        if(blood.isEmpty())
+            return false;
+
+        Blood currBlood = blood.get();
+        int value = currBlood.getSupply() - dto.getQuantity();
+        
+        if(value < 0) return false;
+        currBlood.setSupply(value);
+        bloodRepo.save(currBlood);
+
+        return true;
+	}
 }
