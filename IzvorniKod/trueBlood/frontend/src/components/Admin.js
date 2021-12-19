62e169499d4b541a59f555c0562f52fa353e2cc7
Josip Pardon <josip.pardon@gmail.com>
2021-12-19 17:29:44 +0100

    Osnovni kostur za adminov izbornik dodan

--- IzvorniKod/trueBlood/frontend/src/components/Admin.js
+++ IzvorniKod/trueBlood/frontend/src/components/Admin.js
@@ -5,30 +5,48 @@ import EmployeesList from "./AdminComponents/EmployeesList";
 import Limits from "./AdminComponents/Limits";
 import DonorsList from "./CommonComponents/DonorsList";
 import DonorData from "./CommonComponents/DonorData";
+import { Tab, Row, Col, Nav, Sonnet } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Donor.css';
 
 function Admin(props) {
 
     const [view, setView] = React.useState("myData");
 
+        
     return(
         <div>
-            <div>
-                <button onClick={() => setView("employees")}>Djelatnici</button>
-                <button onClick={() => setView("donors")}>Donori</button>
-                <button onClick={() => setView("limits")}>Granice</button>
-            </div>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                <Row>
+                    <Col className="col-1">
+                        <Nav variant="pills" className="flex-column">
+                            <Nav.Item className="red" >
+                                <Nav.Link eventKey="first">Djelatnici</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Donori</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">Granice</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                    <Col className="col-10">
+                        <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <EmployeesList/>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <DonorsList/>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <Limits/>
+                                </Tab.Pane>
+                        </Tab.Content>
+                    </Col>
+                </Row>
+            </Tab.Container>
 
-            <div hidden={!(view==="employees")}>
-                <EmployeesList/>
-            </div>
-
-            <div hidden={!(view==="donors")}>
-                <DonorsList/>
-            </div>
-
-            <div hidden={!(view==="limits")}>
-                <Limits/>
-            </div>
         </div>
 
        
