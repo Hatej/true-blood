b50d3da042234d778f8d38be31559e1f4712b295
Josip Pardon <josip.pardon@gmail.com>
2022-01-12 13:21:49 +0000

    Dodan još jedan test u SeleniumTests.java

--- IzvorniKod/trueBlood/src/test/java/fer/progi/illidimusdigitus/trueblood/SeleniumTests.java
+++ IzvorniKod/trueBlood/src/test/java/fer/progi/illidimusdigitus/trueblood/SeleniumTests.java
@@ -21,38 +21,44 @@ public class SeleniumTests {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Chrome Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver(); 
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    
+		  System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Chrome Driver\\chromedriver.exe");
+		  WebDriver driver = new ChromeDriver(); 
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		    
 
-        boolean result;
-       
-        result = loginTest(driver, "admin", "admin", "admin");
-        System.out.println("Test pass: " + result);
-        
-        result = loginTest(driver, "djelatnik", "djelatnik", "djelatnik");
-        System.out.println("Test pass: " + result);
-        
-        result = loginTest(driver, "donor", "donor", "donor");
-        System.out.println("Test pass: " + result);
+	        boolean result;
+	       
+	        result = loginTest(driver, "admin", "admin", "admin");
+	        System.out.println("Test pass: " + result);
+	        
+	        result = loginTest(driver, "djelatnik", "djelatnik", "djelatnik");
+	        System.out.println("Test pass: " + result);
+	        
+	        result = loginTest(driver, "donor", "donor", "donor");
+	        System.out.println("Test pass: " + result);
 
-        result = sendBlood(driver, 2);
-        System.out.println("Test pass: " + result);
-        
-        result = changeBloodLimit(driver, 2, "350", "150");
-        System.out.println("Test pass: " + result);
-        
-        result = makeDonation(driver);
-        System.out.println("Test pass: " + result);
-        driver.quit();
+	        result = sendBlood(driver, 2);
+	        System.out.println("Test pass: " + result);
+	        
+	        result = changeBloodLimit(driver, 2, "350", "150");
+	        System.out.println("Test pass: " + result);
+	        
+	        result = changeBloodLimit(driver, 2, "55", "-5");
+	        System.out.println("Test pass: " + result);
+	        
+	        result = makeDonation(driver);
+	        System.out.println("Test pass: " + result);
+	        driver.quit();
 
-      
-    }
+	}
+
+
+	
+	
 	
 	//ispitivanje slanja krvi
+	
 	private static boolean sendBlood(WebDriver driver, int bloodIndex) {
-		
 		
 		driver.get("http://localhost:3000/login");
 		WebElement element;
@@ -65,25 +71,17 @@ public class SeleniumTests {
         
       //submit (login)
         driver.findElement(By.cssSelector("button[type='submit']")).click();
-        
         waitMiliseconds(2000);
-        
-        driver.findElement(By.cssSelector("#root > div:nth-child(1) > nav > div.navbar-nav.justify-content-end.ms-auto > div:nth-child(1) > a")).click();
-        
-       
-        
+        driver.findElement(By.cssSelector("#root > div:nth-child(1) > nav > div.navbar-nav.justify-content-end.ms-auto > div:nth-child(1) > a")).click();  
         driver.findElement(By.cssSelector("#root > div.App > div > div > div.col-md-2.mb-1.col > div > div:nth-child(3)")).click();
-        
-        
-        
         //ovo je zahjevalo dodatni import gore
-        Select dropdown = new Select(driver.findElement(By.cssSelector("#left-tabs-example-tabpane-third > div > div > div.container.col-md-3.border.border-danger.rounded > form > div:nth-child(1) > div > select")));
+        Select dropdown = new Select(
+        		driver.findElement(By.cssSelector("#left-tabs-example-tabpane-third > div > div > div.container.col-md-3.border.border-danger.rounded > form > div:nth-child(1) > div > select")));
         waitMiliseconds(1000);
         dropdown.selectByIndex(bloodIndex);
         
         element = driver.findElement(By.cssSelector("#left-tabs-example-tabpane-third > div > div > div.container.col-md-3.border.border-danger.rounded > form > div:nth-child(2) > div:nth-child(1) > input"));
         element.sendKeys("1");
-        
         element = driver.findElement(By.cssSelector("#left-tabs-example-tabpane-third > div > div > div.container.col-md-3.border.border-danger.rounded > form > div:nth-child(2) > div:nth-child(2) > input"));
         element.sendKeys("TESTNA_INSTITUCIJA");
         
@@ -99,10 +97,8 @@ public class SeleniumTests {
         dropdown.selectByIndex(bloodIndex);
         
         text = driver.findElement(By.cssSelector("#left-tabs-example-tabpane-third > div > div > div.col > p:nth-child(5)")).getText();
-        String trenutnaZaliha = text.substring(text.indexOf(":")+2);
-               
+        String trenutnaZaliha = text.substring(text.indexOf(":")+2);    
         return nakonSlanja.equals(trenutnaZaliha);
-            
 	}
 
 
@@ -110,11 +106,9 @@ public class SeleniumTests {
 	public static boolean loginTest(WebDriver driver, String username, String password, String role) {
 		
 		driver.get("http://localhost:3000/login");
-		
 		// Find the username input element by its name
 		WebElement element;
 	    element = driver.findElement(By.name("username"));        
-
         // Enter username
         element.sendKeys(username);
        
@@ -126,8 +120,6 @@ public class SeleniumTests {
         driver.findElement(By.cssSelector("button[type='submit']")).click();
         
         waitMiliseconds(2000);
-        
-        
         element = driver.findElement((By.cssSelector("#root > div:nth-child(1) > nav > div.navbar-nav.justify-content-end.ms-auto > div:nth-child(4) > a")));
         
         if(element.getText().equals(role))
@@ -145,18 +137,12 @@ public class SeleniumTests {
 		WebElement element;
 	    element = driver.findElement(By.name("username")); 
         element.sendKeys("admin");
-       
-        //Find password element and enter password
         element = driver.findElement(By.name("password"));
         element.sendKeys("admin");
         
-        
         driver.findElement(By.cssSelector("button[type='submit']")).click();
-        
         waitMiliseconds(2000);
-        
         driver.findElement(By.cssSelector("#root > div:nth-child(1) > nav > div.navbar-nav.justify-content-end.ms-auto > div:nth-child(1) > a")).click();
-        
         driver.findElement(By.cssSelector("#root > div.App > div > div > div.col-md-2.mb-1.col > div > div:nth-child(3)")).click();
         
         Select dropdown = new Select(driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col-md-6 > select")));
@@ -165,29 +151,30 @@ public class SeleniumTests {
         
         element = driver.findElement(By.name("upperbound"));
         element.sendKeys(upperbound);
-        
         element = driver.findElement(By.name("lowerbound"));
         element.sendKeys(lowerbound);
         
         driver.findElement(By.cssSelector("button[type='submit']")).click();
         waitMiliseconds(1000);
-        driver.switchTo().alert().accept();
-        
-        dropdown = new Select(driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col-md-6 > select")));
-        waitMiliseconds(1000);
-        dropdown.selectByIndex(bloodIndex);
-        
-        String text = driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col > p:nth-child(2)")).getText();
-        String gornjaGranica = text.substring(text.indexOf(":")+2);
-        
-        text = driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col > p:nth-child(3)")).getText();
-        String donjaGranica = text.substring(text.indexOf(":")+2);
-        
-        
-        return gornjaGranica.equals(upperbound) && donjaGranica.equals(lowerbound);
-        
-	
+        if (Integer.parseInt(upperbound) >= 0 && Integer.parseInt(lowerbound) >= 0) {
+        	 driver.switchTo().alert().accept();
+             dropdown = new Select(driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col-md-6 > select")));
+             waitMiliseconds(1000);
+             dropdown.selectByIndex(bloodIndex);
+             
+             String text = driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col > p:nth-child(2)")).getText();
+             String gornjaGranica = text.substring(text.indexOf(":")+2);
+             text = driver.findElement(By.cssSelector("#parent > form > div:nth-child(1) > div.col > p:nth-child(3)")).getText();
+             String donjaGranica = text.substring(text.indexOf(":")+2);
+            
+             return gornjaGranica.equals(upperbound) && donjaGranica.equals(lowerbound);
+        } else {
+        	String textAlert = driver.findElement(By.cssSelector("#parent > form > div.mb-3.mt-3.row > div > div")).getText();
+        	return textAlert.equals("Granice moraju biti pozitivne!");
+        }
 	}
+	
+	
 	
 	
 	
@@ -229,10 +216,11 @@ public class SeleniumTests {
         String alertText = driver.switchTo().alert().getText();
         driver.switchTo().alert().accept();
         
-        return alertText.equals("Donacija evidentirana");
+        return alertText.equals("Donacija evidentirana") || alertText.equals("Nije moguće donirati! Nije prošlo dovoljno vremena od zadnje donacije!");
         
         
 	}
 
 	
 }
+
